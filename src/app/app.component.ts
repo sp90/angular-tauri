3bfd7f98b09baac3f18d8b3643224c819c2d7f25
Simon Dragsbæk <dev@nomis.dk>
2023-05-15 11:32:17 +0200

    More cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { APP_CONFIG } from '../environments/environment';
-import { TauriService } from './core/services';
+import { TauriService } from './core/services/tauri/tauri.service';
 
 @Component({
   selector: 'app-root',
